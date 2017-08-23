43cf8e3e177754b44a0c36f9cfe0fcde5bac70a2
abdullatif123 <abdul1997latif@gmail.com>
2017-08-23 16:16:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,11 +71,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:k',function(req,res){
+app.get('/:articleName',function(req,res){
     //articleName == article one
     //articles[articleone]={} content object of article one
     var articleName=req.params.articleName;
-    res.send(createTemplate(articles[k]));
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
