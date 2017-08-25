c69eb21cfe52b75ae23c8d5059069021ff229e1e
abdullatif123 <abdul1997latif@gmail.com>
2017-08-25 17:55:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 //change the text of main text div
 
-var element=document.getElementById('main-text');
-element.innerHTML="New Value";
+//var element=document.getElementById('main-text');
+//element.innerHTML="New Value";
 //move the image
   var img=document.getElementById("madi");
   var marginLeft=0;
