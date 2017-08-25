8350f21f4c589721e0fcb9bdd4993052c790174c
abdullatif123 <abdul1997latif@gmail.com>
2017-08-25 13:08:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element=document.getElementById('main-text');
 element.innerHTML="New Value";
 //move the image
   var img=document.getElementById("madi");
-  img.onclick= function(){
-      img.style.marginleft='100px';
+  var marginLeft=0;
+  function moveRight(){
+      marginLeft=marginLeft+10;
+      img.style.marginLeft=marginLeft+'px';
+  }
+  img.onclick = function(){
+      var interval=setInterval(moveRight,100);
+     
   };
