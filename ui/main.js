0b3cfedd7006850f6013694a6cf8319bc1e57351
abdullatif123 <abdul1997latif@gmail.com>
2017-08-25 20:54:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,10 +27,19 @@ var counter=0;
  var name=nameInput.value;
  var submit=document.getElementById('submit_btn');
  submit.onclick = function () {
-     //Make a request to the server and send the name
+       //create a request object
+     var request=new XMLHttpRequest();
+     
+     //capture the response and store it in a variable
+     request.onreadystatechange=function(){
+         if(request.readyState===XMLHttpRequest.DONE){
+             //Take some action
+             if(request.status===200){
+               //Make a request to the server and send the name
      
      //capture a list of names and render it as a list
-     var names=["name 1","name 2"];
+     var names=request.responseText;
+     names=JSON.parse(names);
      var list='';
      for(var i=0 ; i<names.length ;i++)
      {
@@ -39,4 +48,13 @@ var counter=0;
      }
      var ul=document.getElementById("namelist");
      ul.innerHTML=list;
+             }
+         }
+       //Not done yet  
+     };
+     // Make ther request
+     request.open('GET','http://abdul1997latif.imad.hasura-app.io/submit-name=' + name,true);
+     request.send(null);
+     
+    
  };
