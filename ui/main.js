addfa88447ee9207d57d2acc42b83a638baeee05
abdullatif123 <abdul1997latif@gmail.com>
2017-08-25 13:00:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ var element=document.getElementById('main-text');
 element.innerHTML="New Value";
 //move the image
   var img=document.getElementById("madi");
-  img.onclick=function(){
-      img.style.marginleft="100px";
+  img.onclick= function(){
+      img.style.marginleft='100px';
   };
